0fa44f48cb20a9bfc337a41dfe64dc5858c8609c
davidDSuiza <davidflores@dsuiza.com.ar>
2022-09-28 16:00:15 -0300

    Feature: routes, pages and components added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Detail from './pages/Detail/Detail';
+import Error from './pages/Error/Error';
+
 const App = () => {
   return (
     <>
-      Cripto Tuc
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/detail/:id' element={<Detail />} />
+        <Route path='/*' element={<Error />} />
+      </Routes>
     </>
   );
 };
