76157023d6e4d14d5937ccb165e7c084475dacba
David <david.jesus.sebastian.flores@gmail.com>
2022-09-29 23:00:37 -0300

    Feature: coin table and history added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top'>
+      <p className='col-md-4 mb-0 text-muted'>© 2022 Fiona Company, Inc</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <nav className='navbar navbar-dark bg-dark'>
+      <div className='container'>
+        <NavLink className='navbar-brand fs-4' to='/'><i className='bi bi-currency-bitcoin me-1'></i>CoinFiona</NavLink>
+        <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div className='collapse navbar-collapse d-flex justify-content-between' id='navbarNav'>
+          <ul className='navbar-nav'>
+            <li className='nav-item'><NavLink className='nav-link' to='/'>Inicio</NavLink></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
