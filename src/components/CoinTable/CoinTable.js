a1430eb55c979889a487c0a733d6a52ec118d6c4
davidDSuiza <davidflores@dsuiza.com.ar>
2022-09-30 13:11:15 -0300

    Feature: added number formatting

--- src/components/CoinTable/CoinTable.js
+++ src/components/CoinTable/CoinTable.js
@@ -7,6 +7,9 @@ const CoinTable = ({ currentItems }) => {
   const handleClick = (id) => {
     navigate('/detail', { state: { coinId: id } });
   };
+  const numberWithCommas = (decimalNumber) => {
+    return decimalNumber.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  };
   return (
     <table className='table table-dark table-hover'>
       <thead className='tbl-header'>
@@ -20,15 +23,17 @@ const CoinTable = ({ currentItems }) => {
       </thead>
       <tbody>
         {currentItems
-          && currentItems?.map((item) => (
+          && currentItems?.map((item) => {
+            const profit = item.price_change_percentage_24h > 0;
+            return (
             <tr key={item.id} onClick={ () => handleClick(item.id)}>
               <th scope="row">{item.market_cap_rank}</th>
               <td><img className='coin-logo' src={item.image}/> {item.name} <span className='coin-symbol'>{item.symbol.toUpperCase()}</span></td>
-              <td>{item.current_price}</td>
-              <td>{item.price_change_percentage_24h}</td>
-              <td>{item.market_cap}</td>
-            </tr>
-          ))}
+              <td>{numberWithCommas(item.current_price.toFixed(2))}</td>
+              <td className={ profit > 0 ? 'tbl-cell-green' : 'tbl-cell-red'}>{profit && '+'} {item.price_change_percentage_24h.toFixed(2)}%</td>
+              <td>{numberWithCommas(item.market_cap.toString().slice(0, -6))} M</td>
+            </tr>);
+          })}
       </tbody>
     </table>
   );
