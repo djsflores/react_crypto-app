9b57b5704b89be1553b31bdf58cebf06e22be5b3
David Jesús Sebastián Flores <djsflores@Davids-MacBook-Pro.local>
2022-10-03 00:59:50 -0300

    Feature: coverage+

--- src/components/CoinHistory/CoinHistory.test.js
+++ src/components/CoinHistory/CoinHistory.test.js
@@ -1,11 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import CoinHistory from './CoinHistory';
 import store from '../../app/store';
 
+window.ResizeObserver = function () {
+  return {
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  };
+};
+
 describe('CoinHistory test', () => {
   test('Esperamos que renderice el componente CoinHistory', () => {
-    render(<Provider store={store}><CoinHistory /></Provider>);
+    render(<Provider store={store}><Router><CoinHistory /></Router></Provider>);
     const element = screen.getByText('Grafico de Precios (USD)');
     expect(element).toBeInTheDocument();
   });
