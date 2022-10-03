00c86f38fca4653be82aed87bfc0acf02aef1b29
David Jesús Sebastián Flores <djsflores@Davids-MacBook-Pro.local>
2022-10-03 00:05:05 -0300

    Feature: coverage

--- src/components/Footer/Footer.test.js
+++ src/components/Footer/Footer.test.js
@@ -1,9 +1,11 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import Footer from './Footer';
+import store from '../../app/store';
 
 describe('Footer test', () => {
   test('Esperamos que renderice el componente Footer', () => {
-    render(<Footer />);
+    render(<Provider store={store}><Footer /></Provider>);
     const element = screen.getByText('© 2022 Fiona Company, Inc');
     expect(element).toBeInTheDocument();
   });
